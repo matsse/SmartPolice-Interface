e8736337c60f22408348db6cfb39fa522a088535
matsse <56912685+matsse@users.noreply.github.com>
2020-05-18 14:52:56 +0200

    updated action core

--- Core/Actions.go
+++ Core/Actions.go
@@ -7,4 +7,26 @@ package Core
 
 
 
-func 
+// Reads an input action command and dispatches the correct method
+func ReadAction(method string) (interface{}, error) {
+	
+	
+	
+	
+	
+	
+	
+	return nil, nil
+}
+
+// Runs an action
+func RunAction() {
+
+}
+
+// Runs a chain of actions
+func RunChain() {
+
+
+}
+

0b179f9da677b327a29e08644eafff596ce7262b
matsse <56912685+matsse@users.noreply.github.com>
2020-05-22 09:10:00 +0200

    Fixed file writing error
    Weird bug where the write to file function either appended a new JSON ontop of an existing one

--- Core/Actions/FileIO.go
+++ Core/Actions/FileIO.go
@@ -2,38 +2,67 @@ package Actions
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"os"
 )
 
-func SaveToFile(in interface{}, name string) (error){
-	
-	
-	if _, err := os.Stat("../DataOutput/"+name+".json"); err == nil  {
-		//log.Fatal("File already exist!")
-		//return err
-		os.Remove("../DataOutput/"+name+".json")
 
+
+
+
+func SaveToFile(in interface{}, name string) (error){
+	if _, err := os.Stat("../DataOutput/"+name+".json"); err == nil  {
+	
 	}
-	file, err := os.Create("../DataOutput/"+name+".json")
-	if err != nil {
-		return err
-	}
+
+	
+	file, err := os.OpenFile("../DataOutput/"+name+".json", os.O_RDWR, 0755)
 	defer file.Close()
 	
-	in = GenerateOutput(in)
-	
-	x, jerr := json.MarshalIndent(in, "", "\t")
-	if jerr != nil {
-		return err
+	if x, _ := file.Stat(); x.Size() < 10 {
+		in = GenerateOutput(in)
+		
+		x, jerr := json.MarshalIndent(in, "", "\t")
+		if jerr != nil {
+			return jerr
+		}
+		
+		_, err = file.Write(x)
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		
+		var temp map[string] interface{}
+		buffer, _ := ioutil.ReadAll(file)
+		
+		error2 := json.Unmarshal(buffer, &temp)
+		
+		
+		
+		if error2 != nil {
+			panic(error2)
+		}
+		output := AppendOutput(in, temp)
+		x, jerr := json.MarshalIndent(output, "", "\t")
+
+		if jerr != nil {
+			return jerr
+		}
+		file.Close()
+		file, _ = os.Create("../DataOutput/"+name+".json")
+		
+		_, writeErr := file.Write(x)
+		if writeErr != nil {
+			panic(writeErr)
+		}
+		
 	}
-	
-	
-	file.Write(x)
-	
-	
 	
 	return nil
 }
+
 
 
 
@@ -45,10 +74,14 @@ func GenerateOutput(in interface{}) map[string]interface{}  {
 		"entry1" : in,
 	}
 	
-	
-	
-	
 	return temp
+}
+
+func AppendOutput(in interface{},  file map[string]interface{}) map[string]interface{}  {
+	count := len(file["data"].(map[string]interface{}))
+	entry := fmt.Sprintf("entry%d", count+1)
 	
+	file["data"].(map[string]interface{})[entry] = in
 	
+	return file
 }
